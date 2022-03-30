2a74994ee0e7ad74a1985fbb736c73202611fe8d
ilyasssklimov <ilyasssklimov@gmail.com>
2022-03-30 10:36:28 +0300

    delete setf & let from lab_05

--- lab_05/src/main.lisp
+++ lab_05/src/main.lisp
@@ -1,16 +1,16 @@
-; task 1
+;;; task 1
 (defun palindrome (lst)
   (cond ((null (listp lst)) NIL)
 	(T (equal lst (reverse lst)))))
 
 
-; task 2
+;;; task 2
 (defun set_equal (lst1 lst2)
   (and (listp lst1) (listp lst2)
        (subsetp lst1 lst2) (subsetp lst2 lst1)))
 
 
-; task 3
+;;; task 3
 (defun capital_by_country (table country)
   (cond ((null table) NIL)
 	((equal (caar table) country) (cdar table))
@@ -22,79 +22,70 @@
 	(T (country_by_capital (cdr table) capital))))
 
 
-; task 4
+;;; task 4
 (defun swap_first_last (lst)
-  (if (null (listp lst)) NIL)
-  (let ((first_elem (car lst)))
-    (setf (car lst) (car (last lst)))
-    (setf (car (last lst)) first_elem)
-    lst))
+  (cond ((< (length lst) 2) lst)
+	(T (append (last lst) (butlast (cdr lst)) (list (car lst))))))
 
 
-; task 5
+;;; task 5
+(defun butfirst (lst num)
+  (car (remove NIL (maplist #'(lambda (x)
+				(if (= (length x) (- (length lst) num))
+				    x NIL)) lst))))
+
+(defun swap_two_elements_help (lst i j)
+  (cond ((or (< (length lst) 2) (= i j)) lst)
+	(T (append (butlast lst (- (length lst) i))
+		   (list (nth j lst))
+		   (butlast (butfirst lst (+ i 1)) (- (length lst) j))
+		   (list (nth i lst))
+		   (butfirst lst (+ j 1))))))
+
 (defun swap_two_elements (lst i j)
-  (if (null (listp lst)) NIL)
-  (let ((i_elem (nth i lst)))
-    (setf (nth i lst) (nth j lst))
-    (setf (nth j lst) i_elem)
-    lst))
+  (cond ((< j i) (swap_two_elements_help lst j i))
+	(T (swap_two_elements_help lst i j))))
 
 
-; task 6
-; 1 2 3 4 5 -> 2 3 4 5 1
+;;; task 6
+;;; 1 2 3 4 5 -> 2 3 4 5 1
 (defun swap_to_left (lst)
-  (cond ((null (listp lst)) NIL)
-	((< (length lst) 2) lst)
-	(T (let ((first_elem (first lst)))
-	     (setf (car lst) (second lst))
-	     (setf (cdr lst) (append (nthcdr 2 lst) (list first_elem)))
-	     lst))))
+  (cond ((< (length lst) 2) lst)
+	(T (append (butfirst lst 1) (list (car lst))))))
 
-; 1 2 3 4 5 -> 5 1 2 3 4
+;;; 1 2 3 4 5 -> 5 1 2 3 4
 (defun swap_to_right (lst)
-  (cond ((null (listp lst)) NIL)
-	((< (length lst) 2) lst)
-	(T (let ((last_elem (car (last lst))))
-	     (setf (cdr lst) (butlast lst))
-	     (setf (car lst) last_elem)
-	     lst))))
+  (cond ((< (length lst) 2) lst)
+	(T (append (last lst) (butlast lst)))))
 
 
-; task 7
+;;; task 7
 (defun add_two_element_list (lst new_pair)
   (cond ((null (listp lst)) NIL)
 	((null (notany #'(lambda (pair) (equal pair new_pair)) lst)) NIL)
 	(T (nconc lst (list new_pair)))))
 
 
-; task 8
-; all elements are numbers
+;;; task 8
+;;; all elements are numbers
 (defun mul_first_number_v1 (lst k)
-  (cond ((or (null (listp lst)) (null (numberp k))) NIL)
-	(T (setf (car lst) (* (car lst) k))))
-  lst)
+  (append (list (* (car lst) k)) (cdr lst)))
 
-; all elements are objects
+;;; all elements are objects
 (defun mul_first_number_v2 (lst k)
-  (cond ((or (null (listp lst)) (null (numberp k))) NIL)
-	(T (let ((stop 0)
-		 (i 0))
-	     (mapcar #'(lambda (element) (if (and (numberp element) (= stop 0))
-					    (setf stop 1
-						  (nth i lst) (* element k))
-					    (setf i (+ i 1)))) lst))
-	   lst)))
+  (let ((stop 0))
+    (mapcar #'(lambda (x) (if (and (= stop 0) (numberp x))
+			      (and (setf stop 1) (* x k))
+			      x)) lst)))
 
 
-; task 9
+;;; task 9
+(defun select_between_help (lst left right)
+  (sort (remove NIL (mapcar #'(lambda (num)
+				(if (and (> num left) (< num right)) num))
+			    lst)) #'<))
+
 (defun select_between (lst left right)
-  (cond ((or (null (listp lst))
-	     (null (numberp left)) (null (numberp right))) NIL)
-	(T (if (> left right) (let ((tmp left))
-				(setf left right)
-				(setf right tmp)))
-	   (sort (remove NIL (mapcar #'(lambda (num)
-					 (if (and (> num left) (< num right)) num))
-				     lst))
-		 #'<))))
+  (cond ((< right left) (select_between_help lst right left))
+	(T (select_between_help lst left right))))
 
